76b733bfcfa24abb963dce49e71c539ada2d539b
nousantx <nousantx@gmail.com>
2024-04-18 06:23:52 +0700

    feat: Project showcase

--- src/components/projects.ts
+++ src/components/projects.ts
@@ -20,10 +20,39 @@ export const projects: ProjectProps[] = [
   {
     title: "tenoxui/css documentation",
     desc: "Complete documentation of TenoxUI CSS Framework.",
-    thumb: "https://tenoxui.web.app/img/tenoxui_social_card.jpg",
+    thumb: "./img/tenoxui.jpeg",
     tags: ["docusaurus", "blog", "docs", "reactjs"],
     live: "https://tenoxui.web.app",
     github: "https://github.com/nousantx/tenoxui-website",
+  },
+  {
+    title: "Cliff UI Components Library",
+    desc: "Collection of react components built with TenoxUI CSS Framework.",
+    thumb: "./img/cliffui.jpeg",
+    tags: ["typescript", "open-source", "react"],
+    live: "https://cliffui.web.app",
+    github: "https://github.com/nousantx/Cliff-UI",
+  },
+  {
+    title: "TodoList App",
+    desc: "Every programmer must have project like this.",
+    thumb: "./img/todolist_project.jpg",
+    tags: ["docusaurus", "blog", "docs", "reactjs"],
+    live: "https://tenoxui.web.app",
+    github: "https://github.com/nousantx/tenoxui-website",
+  },
+  {
+    title: "Ramadhan 1445H",
+    desc: "Fun project, Ramadhan landing page, schedule, and more.",
+    thumb: "./img/ramadhan.jpeg",
+    tags: ["html/css", "landing-page"],
+    live: "https://nousantx.github.io/Ramadhan-1445H/",
+    github: "https://github.com/nousantx/Ramadhan-1445H",
+  },
+  {
+    title: "tenk7css",
+    desc: "My first CSS framework",
+    github: "https://github.com/nousantx/tnk7css",
   },
   {
     title: "NFTs Landing Page Design",
@@ -39,6 +68,7 @@ export const projects: ProjectProps[] = [
     tags: ["design", "ui/ux", "landing-page"],
     thumb:
       "https://cdn.dribbble.com/userupload/12671265/file/original-170d34a82ee6a020aa3b93af6a1299d4.png?crop=0x0-1366x1024&resize=400x300&vertical=center",
+    live: "https://nousantx.github.io/tenoxui-pages/design-day3",
     design: "https://dribbble.com/shots/23539932-NFTs-simple-Homepage-design",
   },
 ];
